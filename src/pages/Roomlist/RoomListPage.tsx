79e84aaaf24f61a872da0a7ee24a609541edf1f5
Khang Nguyen <85474081+khangnguyen5843@users.noreply.github.com>
2022-10-10 16:56:26 +0700

    update component roomlistitem

--- src/pages/Roomlist/RoomListPage.tsx
+++ src/pages/Roomlist/RoomListPage.tsx
@@ -16,7 +16,7 @@ const RoomListPage = () => {
             <p>Hơn 1.000 nhà ở</p>
             <div>Bộ lọc</div>
           </div>
-          <div className="roomlist-item_content">
+          <div className="roomlist-item_content" style={{display:'flex', width:"100%", flexWrap: 'wrap'}}>
             {arr.map((item, index) => (
               <div key={index} style={{ width: '50%', marginTop: 20 }}>
                 <RoomListItem />
@@ -24,8 +24,8 @@ const RoomListPage = () => {
             ))}
           </div>
         </div>
-        <div className="roomlist-map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.455492242454!2d106.66801531433677!3d10.776383562142328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fef16459c77%3A0xf9ba55fa4f33e17d!2zMTZBIEzDqiBI4buTbmcgUGhvbmcsIFBoxrDhu51uZyAxMiwgUXXhuq1uIDEwLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1665331609460!5m2!1svi!2s" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <div className="roomlist-map" style={{width:"55%"}}>
+          <iframe style={{width:"100%", height:"100%", border:0}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.455492242454!2d106.66801531433677!3d10.776383562142328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fef16459c77%3A0xf9ba55fa4f33e17d!2zMTZBIEzDqiBI4buTbmcgUGhvbmcsIFBoxrDhu51uZyAxMiwgUXXhuq1uIDEwLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1665331609460!5m2!1svi!2s" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
       <FooterPage />
