5da972f44f5efd3d6185fbb4fe46e493bceb52bf
Khang Nguyen <85474081+khangnguyen5843@users.noreply.github.com>
2022-10-11 16:37:50 +0700

    update LogoHeard+fix carousel

--- src/component/RoomListItem/RoomListItem.tsx
+++ src/component/RoomListItem/RoomListItem.tsx
@@ -3,22 +3,24 @@ import './RoomListItem.css'
 
 const RoomListItem = () => {
   return (
-    <div className="roomlist-item">
-      <img className="roomlist-item_img" src="https://kientruccb.vn/wp-content/uploads/2020/02/mau-thiet-ke-nha-dep-truyen-cam-hung8.jpg" alt="" />
-      <div className="roomlist-item_name">
-        <h3>Nhà tại Výloboky, ukraina</h3>
+    <div className="roomlist-item" style={{margin:"0px 10px", position:'relative'}}>
+      <img className="roomlist-item_img" style={{width:"100%",borderRadius:"10px",cursor:'pointer'}} src="https://kientruccb.vn/wp-content/uploads/2020/02/mau-thiet-ke-nha-dep-truyen-cam-hung8.jpg" alt="..." />
+      <div className="roomlist-item_name" style={{display:'flex',justifyContent:'space-between',alignItems:'center', marginTop:"10px"}}>
+        <h3 style={{fontSize:"15px"}}>Nhà tại Výloboky, ukraina</h3>
         <p>5,0 (51)</p>
       </div>
       <div className="roomlist-item_des">
-        <p>Taxi Grove</p>
-        <p>1 giường queen</p>
-        <p>Ngày 30 tháng 10 - Ngày 04 tháng 11</p>
+        <p style={{color:"#746b6b",margin:"0px 0px"}}>Taxi Grove</p>
+        <p style={{color:"#746b6b",margin:"0px 0px"}}>1 giường queen</p>
+        <p style={{color:"#746b6b",margin:"0px 0px"}}>Ngày 30 tháng 10 - Ngày 04 tháng 11</p>
       </div>
-      <p className="roomlist-item_price">$79 <span>đêm</span></p>
-      <div className="roomlist-icon_left">
+      <p className="roomlist-item_price" style={{fontSize:"15px"}} >$79 <span>đêm</span></p>
+      <div className="roomlist-icon_left" style={{position:'absolute',top:"15px",left:"15px",borderRadius:"5px",backgroundColor:"#fafafa",padding:"5px"}}>
         Chủ nhà siêu cấp
       </div>
-      <div className="roomlist-icon_right"></div>
+      <div className="roomlist-icon_right" style={{position:"absolute", top:"15px", right: "15px",  width:"20px", height:"20px",opacity:0.8}}>
+        <img width={21 } height={18} src="./img/home/heart.png" alt="..." />
+      </div>
     </div>
   )
 }
