94ad784fecfd4cb6ce2fc8416e12c6db09f628e6
hungdinh233 <dinhtienhung233@gmail.com>
2022-10-27 16:35:31 +0700

    update login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 type Props = {};
 
 export default function Login({}: Props) {
+  const frm = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      //check validation
+      email: Yup.string()
+        .required("Bạn chưa nhập email này!")
+        .email("Email không đúng định dạng"),
+      password: Yup.string().required("Bạn vẫn chưa nhập mật khẩu này!"),
+    }),
+    //values là tên mặc định do formik và yup đặt
+    onSubmit: (values) => {},
+  });
+
   return (
     <div className="container">
-      <div className="sign_in d-flex">
+      <form className="sign_in d-flex" onSubmit={frm.handleSubmit}>
         <div className="sign_in_left"></div>
         <div className="sign_in_right">
           <div className="bg_cover d-flex flex-column align-items-center justify-content-center">
@@ -16,24 +34,33 @@ export default function Login({}: Props) {
                 id="email"
                 placeholder="Tài khoản"
                 className="email_inp"
+                onChange={frm.handleChange}
+                onBlur={frm.handleBlur}
               />
+                <span className="text-danger">{frm.errors.email}</span>
               <input
                 type="password"
                 id="password"
                 placeholder="Mật khẩu"
                 className="password_inp"
+                onChange={frm.handleChange}
+                onBlur={frm.handleBlur}
               />
+              <span className="text-danger">{frm.errors.password}</span>
             </div>
             <div className="action_btn d-flex flex-column align-items-center">
-              <button className="login_btn">Đăng nhập
-              <i className="fa-solid fa-door-open"></i></button>
-              <button className="signin_btn">Đăng ký
-              <i className="fa-solid fa-user-plus"></i>
+              <button className="login_btn">
+                Đăng nhập
+                <i className="fa-solid fa-door-open"></i>
+              </button>
+              <button className="signin_btn">
+                Đăng ký
+                <i className="fa-solid fa-user-plus"></i>
               </button>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
