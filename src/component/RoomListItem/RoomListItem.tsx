0a8f3785c6bce0c86674a8976a3f03df35135230
Khang Nguyen <khang.nguyen@joikid.com>
2022-11-02 01:27:43 +0700

    update

--- src/component/RoomListItem/RoomListItem.tsx
+++ src/component/RoomListItem/RoomListItem.tsx
@@ -1,28 +1,80 @@
-import React from 'react'
-import './RoomListItem.css'
+import React from "react";
+import "./RoomListItem.css";
 
-const RoomListItem = () => {
+const RoomListItem = ({item}: any) => {
+  console.log(item);
+  
   return (
-    <div className="roomlist-item" style={{margin:"0px 10px", position:'relative'}}>
-      <img className="roomlist-item_img" style={{width:"100%",borderRadius:"10px",cursor:'pointer'}} src="https://kientruccb.vn/wp-content/uploads/2020/02/mau-thiet-ke-nha-dep-truyen-cam-hung8.jpg" alt="..." />
-      <div className="roomlist-item_name" style={{display:'flex',justifyContent:'space-between',alignItems:'center', marginTop:"10px"}}>
-        <h3 style={{fontSize:"15px"}}>Nhà tại Výloboky, ukraina</h3>
-        <p>5,0 (51)</p>
+    <div
+      className="roomlist-item"
+      style={{ margin: "0px 10px", position: "relative" }}
+    >
+      <img
+        className="roomlist-item_img"
+        style={{ width: "100%", borderRadius: "10px", cursor: "pointer" }}
+        src="https://kientruccb.vn/wp-content/uploads/2020/02/mau-thiet-ke-nha-dep-truyen-cam-hung8.jpg"
+        alt="..."
+      />
+
+      <div className="roomlist-address">
+        <div className="d-flex justify-content-between align-items-center mt-3">
+          <span style={{fontSize:15}} >{item?.tenPhong}</span>
+
+          <div className="roomlist-like">
+            <div className="d-flex justify-content-between align-items-center">
+              <img
+                src="/img/Star.png"
+                style={{ width: 20, height: 20, marginBottom: 4 }}
+                alt="..."
+              />
+              <span>4,79 (234)</span>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className="roomlist-item_des">
-        <p style={{color:"#746b6b",margin:"0px 0px"}}>Taxi Grove</p>
-        <p style={{color:"#746b6b",margin:"0px 0px"}}>1 giường queen</p>
-        <p style={{color:"#746b6b",margin:"0px 0px"}}>Ngày 30 tháng 10 - Ngày 04 tháng 11</p>
+        <p style={{ color: "#746b6b", margin: "0px 0px" }}>Taxi Grove</p>
+        <p style={{ color: "#746b6b", margin: "0px 0px" }}>1 giường queen</p>
+        <p style={{ color: "#746b6b", margin: "0px 0px" }}>
+          Ngày 30 tháng 10 - Ngày 04 tháng 11
+        </p>
       </div>
-      <p className="roomlist-item_price" style={{fontSize:"15px"}} >$79 <span>đêm</span></p>
-      <div className="roomlist-icon_left" style={{position:'absolute',top:"15px",left:"15px",borderRadius:"5px",backgroundColor:"#fafafa",padding:"5px"}}>
+      <p
+        className="roomlist-item_price"
+        style={{ fontSize: "15px", marginTop: 5 }}
+      >
+
+        <span>$79 đêm</span>
+      </p>
+      <div
+        className="roomlist-icon_left"
+        style={{
+          position: "absolute",
+          top: "15px",
+          left: "15px",
+          borderRadius: "5px",
+          backgroundColor: "#fafafa",
+          padding: "5px",
+        }}
+      >
         Chủ nhà siêu cấp
       </div>
-      <div className="roomlist-icon_right" style={{position:"absolute", top:"15px", right: "15px",  width:"20px", height:"20px",opacity:0.8}}>
-        <img width={21 } height={18} src="./img/home/heart.png" alt="..." />
+      <div
+        className="roomlist-icon_right"
+        style={{
+          position: "absolute",
+          top: "15px",
+          right: "15px",
+          width: "20px",
+          height: "20px",
+          opacity: 0.8,
+        }}
+      >
+        <img width={21} height={18} src="./img/home/heart.png" alt="..." />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RoomListItem
+export default RoomListItem;
--- src/pages/Roomlist/RoomListPage.tsx
+++ src/pages/Roomlist/RoomListPage.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import RoomListItem from '../../component/RoomListItem/RoomListItem'
 import './RoomListPage.css'
 import HeaderPage from '../../component/Header/HeaderPage'
 import FooterPage from '../../component/Footer/FooterPage'
 import { relative } from 'path'
+import axios from 'axios'
+import { getToken } from '../../data/services/token.service'
 
 const RoomListPage = () => {
-  const arr = Array(10).fill(1)
+  
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const url = 'https://airbnbnew.cybersoft.edu.vn/api/phong-thue' 
+    const id = "1"
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAzMCIsIkhldEhhblN0cmluZyI6IjE3LzAyLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NjU5MjAwMDAwMCIsIm5iZiI6MTY0ODIyNzYwMCwiZXhwIjoxNjc2NzM5NjAwfQ.aK-3RvHXQyu6H2-FFiafeSKR4UMCcRmnuDbTT-XIcUU"
+    const getRoomList = async () => {
+      const res = await axios.get(`https://airbnbnew.cybersoft.edu.vn/api/phong-thue/lay-phong-theo-vi-tri?maViTri=1`, {
+        headers : {'Authorization': 'Bearer ' + token}
+      })
+      console.log(res?.data);
+      setData(res?.data?.content)
+    }
+    try {
+      getRoomList()
+    } catch (error) {
+      
+    }
+  },[])
 
   return (
     <>
@@ -15,12 +36,24 @@ const RoomListPage = () => {
         <div className="roomlist-item_" style={{width:"40%", padding:"0px 15px"}}>
           <div className="roomlist-item_title" style={{margin:"24px 0px 8px", display:'flex',alignItems:'center',justifyContent:'space-between'}}>
             <p style={{marginLeft:"10px",marginBottom:"0",padding:0,fontSize:"15px"}}>Hơn 1.000 nhà ở</p>
-            <div style={{marginLeft:"10px",padding:0,fontSize:"15px",marginRight:"10px"}}>Bộ lọc</div>
+
+            <div>
+                  <div style={{padding:"7px 0"}}>
+                    <span className='d-flex border rounded-4' style={{padding:"2px 16px 0"}}>
+                      <span style={{width:"50%"}}>
+                        <img src="./img/setting-lines.png" alt="" style={{width:12,height:12}} />
+                      </span>
+                      <span style={{width:"50%"}}>Bộ lọc</span>
+                    </span>
+              </div>
+              
+            </div>
+            
           </div>
           <div className="roomlist-item_content" style={{display:'flex', width:"100%", flexWrap: 'wrap'}}>
-            {arr.map((item, index) => (
+            {data.map((item, index) => (
               <div key={index} style={{ width: '50%', marginTop: 20 }}>
-                <RoomListItem />
+                <RoomListItem item={item} />
               </div>
             ))}
           </div>
