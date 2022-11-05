d6a11a5490771c595819221d786fd9a7d99e811e
LEDUYPHUONG <boyfriend7777777@gmail.com>
2022-11-05 11:27:08 +0700

    fix(layout/home,admin): install bootstrap and delete router unnecessary and make modalAdmin

--- src/index.tsx
+++ src/index.tsx
@@ -13,16 +13,14 @@ import Home from './pages/Home/Home';
 import Roomlist from './pages/Roomlist/RoomListPage';
 import ResponsiveItem from './HOC/ResponsiveItem/ResponsiveItem';
 import 'antd/dist/antd.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.js';
 import '../src/assets/scss/style.scss';
 import MapHome from './component/MapHome/MapHome';
 import Admin from './pages/Admin/Admin';
 import Profile from './pages/Profile/Profile';
 import Manage from './pages/Admin/Manage';
-import SignIn from './pages/SignIn/SignIn';
 import RoomDetail from './pages/RoomDetail/RoomDetail';
-import SignUp from './pages/SignUp/SignUp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -36,21 +34,15 @@ root.render(
           <Route path='home' element={<ResponsiveItem Component={Home} ComponentMobile={HomeMobile} />}></Route>
           <Route path='roomlist' element={<ResponsiveItem Component={Roomlist} ComponentMobile={Roomlist} />}></Route>
           <Route path='login' element={<Login/>}></Route>
-          <Route path='signup' element={<SignUp/>}></Route>
           <Route path='demo' element={<Demo/>}></Route>
           <Route path='map' element={<MapHome/>}></Route>
-          <Route path='demo' element={<Demo/>}></Route>
           <Route path='profile' element={<Profile/>}></Route>
-          <Route path='detail'>
-            <Route path=':id'></Route>
-          </Route>
-          <Route path='detailroom' element = {<RoomDetail/>}>
-          <Route path=':id'></Route>
+          <Route path='detailroom'>
+            <Route path=':id' element = {<RoomDetail/>}></Route>
           </Route>
           <Route path='listRoom' element = {<Roomlist/>}></Route>
           <Route path='*' element={<Navigate to='' />}></Route>
           <Route path='admin' element={<Admin/>}></Route>
-          <Route path='signin' element={<SignIn/>}></Route>
           <Route path='manage' element={<Manage/>}></Route>
         </Route>
       </Routes>
