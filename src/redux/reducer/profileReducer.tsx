c11cabbd76347ad27104beac73cc790e15a8e4d6
Khang Nguyen <khang.nguyen@joikid.com>
2022-11-05 13:21:14 +0700

    push

--- src/redux/reducer/profileReducer.tsx
+++ src/redux/reducer/profileReducer.tsx
@@ -10,43 +10,63 @@ export interface ProfileRoomModel {
   soLuongKhach: number;
   maNguoiDung: number;
 }
-
-
-
+export interface ProfileUser {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  phone: null;
+  birthday: string;
+  avatar: null;
+  gender: boolean;
+  role: string;
+}
 
 const initialState: any = {
-    arrProfileRoom:[],
+  arrProfileRoom: [],
+  arrProfileUser: [],
 };
 
-
-
-const profileReducer = createSlice ({
-    name:"profileReducer",
-    initialState,
-    reducers: {
-          getProfileRoomAction: (state, action: PayloadAction<ProfileRoomModel[]>) => {
-          state.arrProfileRoom = action.payload;
-        },
+const profileReducer = createSlice({
+  name: "profileReducer",
+  initialState,
+  reducers: {
+    getProfileRoomAction: (state, action: PayloadAction<ProfileRoomModel[]>) => {
+      state.arrProfileRoom = action.payload;
     },
-})
+    getProfileUserAction: (state, action: PayloadAction<ProfileUser[]>) => {
+      state.arrProfileUser = action.payload;
+    },
+  }  
+});
 
-
-export const {getProfileRoomAction} = profileReducer.actions;
+export const { getProfileRoomAction, getProfileUserAction } = profileReducer.actions;
 
 export default profileReducer.reducer;
-
 
 // action api
 
 export const getProfileRoomApi = () => {
-    return async (dispatch: AppDispatch) => {
-      try {
-        const result = await http.get("dat-phong/lay-theo-nguoi-dung/1");
-        let arrProfileRoom: ProfileRoomModel[] = result.data.content;
-        const action = getProfileRoomAction(arrProfileRoom);
-        dispatch(action);
-      } catch (err) {
-        console.log(err);
-      }
-    };
+  return async (dispatch: AppDispatch) => {
+    try {
+      const result = await http.get("dat-phong/lay-theo-nguoi-dung/1");
+      let arrProfileRoom: ProfileRoomModel[] = result.data.content;
+      const action = getProfileRoomAction(arrProfileRoom);
+      dispatch(action);
+    } catch (err) {
+      console.log(err);
+    }
   };
+};
+export const getProfileUserApi = () => {
+  return async (dispacth: AppDispatch) => {
+    try {
+      const result = await http.get("/users");
+      let arrProfileUser: ProfileUser[] = result.data.content;
+      const action = getProfileUserAction(arrProfileUser);
+      dispacth(action);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
